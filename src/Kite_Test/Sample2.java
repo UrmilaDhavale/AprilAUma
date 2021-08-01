4b64da9c53bbdfbd39fa0d02a76f7553659907e6
shri <shri@shri-PC>
2021-08-01 12:17:49 +0530

    [Urmila]Created sample Class

---
+++ src/Kite_Test/Sample2.java
@@ -0,0 +1,9 @@
+package Kite_Test;
+
+public class Sample2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("My Name is \"Urmila\"");
+	}
+}
---
+++ src/Kite_Test/Sample_Kite.java
@@ -0,0 +1,9 @@
+package Kite_Test;
+
+public class Sample_Kite 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Good Aftrenoon");
+	}
+}
